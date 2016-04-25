b7a8bc3aefab33bb2d19d41f0f16ad796a39b0e6
Carlos Malavé <cmalav02@cantv.com.ve>
2016-04-24 22:15:04 -0430

    sss

--- gps_insert.php
+++ gps_insert.php
@@ -5,7 +5,7 @@ $lon = $_REQUEST['longitud'];
 $dis = $_REQUEST['distancia'];
 $imei= $_REQUEST['imei'];
 $idu = $_REQUEST['id_usuario'];
-$q="insert into (latitud,longitud,distancia,imei,id_usuario) value ('$lat','$lon','$dis','$imei','$idu')";
+$q="insert into gpslocalizador (latitud,longitud,distancia,imei,id_usuario) value ('$lat','$lon','$dis','$imei','$idu')";
 $result = mysql_query($q) or die('{"resultado": "Error al Insertar'. mysql_error().'"}');
  header('Content-Type: application/json');
  echo '{"resultado": ['.$result.']}';
