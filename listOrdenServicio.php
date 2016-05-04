3da1b7eba48c6a4b93eeff9a52ebcb3c0dfea56f
Carlos Malavé <cmalav02@cantv.com.ve>
2016-05-03 23:58:01 -0400

    dfdg

--- listOrdenServicio.php
+++ listOrdenServicio.php
@@ -25,7 +25,7 @@ if(!isset($_REQUEST['id_usuario'])){
 		$data[]=$row;
    }
 	if(!isset($_REQUEST['notify'])){
-	$q2=mysql_query("select c.cedula Cedula, c.fullname Cliente, c.email Correo, c.movil Movil,c.telefono Telefono, DATE_FORMAT(b.fecha_registro, '%d-%m-%Y') AS \"Fecha Asignado\", ciudad, direccion,b.codigo,b.id_balanza from clientes c left join direccion_cliente dc on (reg_id=dc.id_cliente) inner join balanzas b on(c.id_balanza=b.id_balanza and b.status=1) inner join usuarios u on (u.id_usuario=b.id_usuario)  where u.id_usuario='$idc'");
+	$q2=mysql_query("select c.cedula Cedula, c.fullname Cliente, c.email Correo, c.movil Movil,c.telefono Telefono, DATE_FORMAT(b.fecha_registro, '%d-%m-%Y') AS \"Fecha Asignado\", ciudad, direccion,b.codigo Orden,'domicilio' as Recepcion,'0' as observacion,b.id_balanza,b.status from clientes c left join direccion_cliente dc on (reg_id=dc.id_cliente) inner join balanzas b on(c.id_balanza=b.id_balanza and b.status=20) inner join usuarios u on (u.id_usuario=b.id_usuario)  where u.id_usuario='$idc'");
 	while ($row=mysql_fetch_object($q2)){
 		$data[]=$row;
 	}
